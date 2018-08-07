970b5e5ee31a611f142be8ad72f13f97e81f4e57
Ahmed Khokar and neil peachey <neil@>
2018-08-07 15:58:19 -0700

    made 19 working flashcards, still need definitions

--- js/scripts.js
+++ js/scripts.js
@@ -3,4 +3,98 @@ $(document).ready(function() {
     $("#javaScriptHidden").toggle();
     $("#javaScriptShowing").toggle();
   });
+
+  $(".operators").click(function() {
+    $("#operatorsHidden").toggle();
+    $("#operatorsShowing").toggle();
+  });
+
+  $(".variables").click(function() {
+    $("#variablesHidden").toggle();
+    $("#variablesShowing").toggle();
+  });
+
+  $(".vnc").click(function() {
+    $("#vncHidden").toggle();
+    $("#vncShowing").toggle();
+  });
+
+  $(".functions").click(function() {
+    $("#functionsHidden").toggle();
+    $("#functionsShowing").toggle();
+  });
+
+  $(".methods").click(function() {
+    $("#methodsHidden").toggle();
+    $("#methodsShowing").toggle();
+  });
+
+  $(".arguments").click(function() {
+    $("#argumentsHidden").toggle();
+    $("#argumentsShowing").toggle();
+  });
+
+  $(".parameters").click(function() {
+    $("#parametersHidden").toggle();
+    $("#parametersShowing").toggle();
+  });
+
+  $(".return").click(function() {
+    $("#returnHidden").toggle();
+    $("#returnShowing").toggle();
+  });
+
+  $(".chaining").click(function() {
+    $("#chainingHidden").toggle();
+    $("#chainingShowing").toggle();
+  });
+
+  $(".strings").click(function() {
+    $("#stringsHidden").toggle();
+    $("#stringsShowing").toggle();
+  });
+
+  $(".booleans").click(function() {
+    $("#booleansHidden").toggle();
+    $("#booleansShowing").toggle();
+  });
+
+  $(".undefined").click(function() {
+    $("#undefinedHidden").toggle();
+    $("#undefinedShowing").toggle();
+  });
+
+  $(".nan").click(function() {
+    $("#nanHidden").toggle();
+    $("#nanShowing").toggle();
+  });
+
+  $(".escape").click(function() {
+    $("#escapeHidden").toggle();
+    $("#escapeShowing").toggle();
+  });
+
+  $(".alert").click(function() {
+    $("#alertHidden").toggle();
+    $("#alertShowing").toggle();
+  });
+
+  $(".comments").click(function() {
+    $("#commentsHidden").toggle();
+    $("#commentsShowing").toggle();
+  });
+
+  $(".jquery").click(function() {
+    $("#jqueryHidden").toggle();
+    $("#jqueryShowing").toggle();
+  });
+
+  $(".attributes").click(function() {
+    $("#attributesHidden").toggle();
+    $("#attributesShowing").toggle();
+  });
+
+
+
+
 });
